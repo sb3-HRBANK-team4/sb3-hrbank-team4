ece8642d41a3b766457a603353e28c222c0650ad
jhlee-codes <jhleezz00@naver.com>
2025-06-05 14:46:30 +0900

    feat: 공통 에러 응답 추가
    - ErrorResponse 추가

---
+++ src/main/java/com/fource/hrbank/dto/common/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.fource.hrbank.dto.common;
+
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String message,
+        String details
+) {
+    public static ErrorResponse of(HttpStatus status, String details) {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                details
+        );
+    }
+
+    public static ErrorResponse of(HttpStatus status, String message, String details) {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                message,
+                details
+        );
+    }
+}
