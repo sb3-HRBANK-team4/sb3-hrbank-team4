a4e393f90fcb9279b7280737321369ff4aed4fc7
hyohyo-zz <akbkck8101@gmail.com>
2025-06-06 14:06:03 +0900

    feat: 직원 전체 조회 + 정렬기능 구현

--- src/main/java/com/fource/hrbank/service/employee/EmployeeService.java
+++ src/main/java/com/fource/hrbank/service/employee/EmployeeService.java
@@ -1,9 +1,15 @@
 package com.fource.hrbank.service.employee;
 
+import com.fource.hrbank.domain.EmployeeStatus;
+import com.fource.hrbank.dto.employee.CursorPageResponseEmployeeDto;
 import com.fource.hrbank.dto.employee.EmployeeDto;
 
 public interface EmployeeService {
 
     public EmployeeDto findById(Long id);
+
+    public CursorPageResponseEmployeeDto findAll(
+        String nameOrEmail, String departmentName, String position, EmployeeStatus status,
+        String sortField, String sortDirection, String cursor, Long idAfter, int size);
 
 }
