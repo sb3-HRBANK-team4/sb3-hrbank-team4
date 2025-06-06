22a3f9058810d3e960b3db309459984cf22938e4
adjoon1 <minj97@naver.com>
2025-06-06 09:53:09 +0900

    feat: ChangeLogDto 생성

---
+++ src/main/java/com/fource/hrbank/dto/changelog/ChangeLogDto.java
@@ -0,0 +1,22 @@
+package com.fource.hrbank.dto.changelog;
+
+import com.fource.hrbank.domain.ChangeType;
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangeLogDto {
+        private Long id;
+        private String employeeNumber;
+        private Instant changedAt;
+        private String changedIp;
+        private ChangeType type;
+        private String memo;
+}
