486da2a9c081a3cb7215674e0492de79749358fe
adjoon1 <minj97@naver.com>
2025-06-08 09:13:30 +0900

    feat: EmployeeRepository에 직원 수 조회 메서드 추가

--- src/main/java/com/fource/hrbank/repository/EmployeeRepository.java
+++ src/main/java/com/fource/hrbank/repository/EmployeeRepository.java
@@ -1,8 +1,12 @@
 package com.fource.hrbank.repository;
 
 import com.fource.hrbank.domain.Employee;
+import com.fource.hrbank.domain.EmployeeStatus;
+import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,5 +17,16 @@ import org.springframework.stereotype.Repository;
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 
 
+    @Query("""
+      SELECT COUNT(e) FROM Employee e
+      WHERE (:status IS NULL OR e.status = :status)
+        AND (:from IS NULL OR e.hireDate >= :from)
+        AND (:to IS NULL OR e.hireDate <= :to)
+    """)
+    long countByFilters(
+        @Param("status") EmployeeStatus status,
+        @Param("from") Date from,
+        @Param("to") Date to
+    );
 }
 
