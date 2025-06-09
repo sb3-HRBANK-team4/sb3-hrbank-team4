7935bfdb6d83c8db37712020c879167aea9927a9
adjoon1 <minj97@naver.com>
2025-06-10 06:26:02 +0900

    refactor: 서비스 인터페이스에 클래스 이름 수정 반영

--- src/main/java/com/fource/hrbank/service/dashboard/DashboardService.java
+++ src/main/java/com/fource/hrbank/service/dashboard/DashboardService.java
@@ -1,9 +1,9 @@
 package com.fource.hrbank.service.dashboard;
 
 import com.fource.hrbank.domain.EmployeeStatus;
-import com.fource.hrbank.dto.dashboard.EmployeeCountResponseDto;
+import com.fource.hrbank.dto.dashboard.EmployeeTrendDto;
 import java.time.LocalDate;
 
 public interface DashboardService {
-    EmployeeCountResponseDto getEmployeeCount(EmployeeStatus status, LocalDate from, LocalDate to);
+    EmployeeTrendDto getEmployeeCount(EmployeeStatus status, LocalDate from, LocalDate to);
 }
