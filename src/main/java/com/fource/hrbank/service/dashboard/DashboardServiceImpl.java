abd695030a000a3b1d4eb714811bc994b95763ae
adjoon1 <minj97@naver.com>
2025-06-10 06:25:45 +0900

    refactor: DashboardServiceImpl 구현체 수정

--- src/main/java/com/fource/hrbank/service/dashboard/DashboardServiceImpl.java
+++ src/main/java/com/fource/hrbank/service/dashboard/DashboardServiceImpl.java
@@ -1,11 +1,12 @@
 package com.fource.hrbank.service.dashboard;
 
 import com.fource.hrbank.domain.EmployeeStatus;
-import com.fource.hrbank.dto.dashboard.EmployeeCountResponseDto;
+import com.fource.hrbank.dto.dashboard.EmployeeTrendDto;
 import com.fource.hrbank.repository.EmployeeRepository;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +15,17 @@ public class DashboardServiceImpl implements DashboardService {
   private final EmployeeRepository employeeRepository;
 
   @Override
-  public EmployeeCountResponseDto getEmployeeCount(EmployeeStatus status, LocalDate from,
-      LocalDate to) {
-      long count = employeeRepository.count(); // countByFilters 문제
-      return new EmployeeCountResponseDto(count);
+  public EmployeeTrendDto getEmployeeCount(EmployeeStatus status, LocalDate from, LocalDate to) {
+    // 현재 기간의 직원 수
+    long count = employeeRepository.count();
+
+    // 전월 대비 값 (임시로 0 지정)
+    long change = 0;
+    double changeRate = 0.0;
+
+    // from이 null이 아니라면 yyyy-MM 형식으로 표시
+    String dateString = (from != null) ? from.toString().substring(0, 7) : "unknown";
+
+    return new EmployeeTrendDto(dateString, count, change, changeRate);
   }
 }
