d13a1dfdb811d2e955337f872984db865165c8de
kangho1870 <ho_gang@naver.com>
2025-06-09 16:36:16 +0900

    chore: 프론트엔드 코드 change

---
+++ src/main/java/com/fource/hrbank/exception/DepartmentDeleteException.java
@@ -0,0 +1,7 @@
+package com.fource.hrbank.exception;
+
+public class DepartmentDeleteException extends RuntimeException {
+  public DepartmentDeleteException(String message) {
+    super(message);
+  }
+}
