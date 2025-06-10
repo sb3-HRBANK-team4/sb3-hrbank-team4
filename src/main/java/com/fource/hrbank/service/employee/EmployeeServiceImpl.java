2c1c2cd7e134d1584ebd3900c70ee953f8dfcf80
kangho1870 <ho_gang@naver.com>
2025-06-10 16:28:23 +0900

    refactor: employeeService.deleteById @Transactional 제거

--- src/main/java/com/fource/hrbank/service/employee/EmployeeServiceImpl.java
+++ src/main/java/com/fource/hrbank/service/employee/EmployeeServiceImpl.java
@@ -328,7 +328,6 @@ public class EmployeeServiceImpl implements EmployeeService {
      *
      * @param id
      */
-    @Transactional
     @Override
     public void deleteById(Long id) {
         Employee employee = employeeRepository.findById(id)
